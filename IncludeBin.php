ba8778090263bf55f8e56433ea241794523fa87a
Eduardo Araújo <99681876+eduardoaraujo006@users.noreply.github.com>
2023-06-07 10:45:58 -0300

    Add files via upload
    7.6.23-10.45

--- IncludeBin.php
+++ IncludeBin.php
@@ -37,13 +37,86 @@ function f_bindec($converter){
         }
     }
 
+# -------------------------------------------------------------------------
+
+# CASO OCTAL
+# CASO EXCEPCIONAL 0
+function f_binoct_excep0(){
+    echo "O número 0 em octal é: 0";
+}
+
+# CASO EXCEPCIONAL 1
+function f_binoct_excep1(){
+    echo "O número 1 em octal é: 1";
+}
+
 # CASO OCTAL
 function f_binoct($converter){
+    $contador = 0;
+    $retorno = $converter;
+    $convertido = [];
 
+    #LAÇO CONVERSOR OCTAL
+    while ($converter >= 2){
+        array_push($convertido, $converter % 8);
+        $converter = intdiv($converter, 8);
+
+        if ($converter == 1){
+            array_push($convertido, $converter);
+        }
+    }
+
+    #BLOCO EXIBIÇÃO
+    $reverso = array_reverse($convertido);
+    $tam = sizeof($reverso, 0);
+    echo "O número: ".$retorno." em octal é: ";
+    while($contador < $tam){
+        echo $reverso[$contador];
+        $contador = ++$contador;
+    }
+}
+
+# -------------------------------------------------------------------------
+
+# CASO EXCEPCIONAL 0
+function f_binhexa_excep0(){
+    echo "O número 0 em binário é: 0";
+}
+
+# CASO EXCEPCIONAL 1
+function f_binhexa_excep1(){
+    echo "O número 1 em binário é: 1";
 }
 
 # CASO HEXADECIMAL
 function f_binhexa($converter){
-    
+    $contador = 0;
+    $retorno = $converter;
+    $convertido = [];
+
+    #LAÇO CONVERSOR HEXADECIMAL
+    while ($converter >= 1){
+        $result = $converter % 16;
+        switch ($result){
+            case $result < 10 and $result >= 1: array_push($convertido, $result); break;
+            case '10': array_push($convertido, 'A'); break;
+            case '11': array_push($convertido, 'B'); break;
+            case '12': array_push($convertido, 'C'); break;
+            case '13': array_push($convertido, 'D'); break;
+            case '14': array_push($convertido, 'E'); break;
+            case '15': array_push($convertido, 'F'); break;
+        }
+        $converter = intdiv($converter, 16);
+    }
+
+    #BLOCO EXIBIÇÃO
+    $reverso = array_reverse($convertido);
+    $tam = sizeof($reverso, 0);
+    echo "O número: ".$retorno." em hexadecimal é: ";
+    while($contador < $tam){
+        echo $reverso[$contador];
+        $contador = ++$contador;
+    }
 }
+
 ?>
--- RegistroBin.php
+++ RegistroBin.php
@@ -20,7 +20,7 @@
 
             switch ($sys){
 
-                #LAÇO CHECKBOX
+                #LAÇO CHECKBOX B
                 case 'B':
 
                     #LAÇO BINÁRIO
@@ -29,15 +29,25 @@
                         case '1': print_r (f_bindec_excep1()); break;
                         default:  print_r (f_bindec($converter));
                     }break;
+
+                #LAÇO CHECKBOX O
                 case 'O': 
 
                     #LAÇO OCTAL
-                    echo 'teste octal'; break;
-
+                    switch ($converter) {
+                        case '0': print_r (f_binoct_excep0()); break;
+                        case '1': print_r (f_binoct_excep1()); break;
+                        default:  print_r (f_binoct($converter));
+                    }break;
                     
+                #LAÇO CHECKBOX H
                 case 'H'; 
                     #LAÇO HEXADECIMAL
-                    echo 'teste hexadecimal'; break;
+                    switch ($converter) {
+                        case '0': print_r (f_binhexa_excep0()); break;
+                        case '1': print_r (f_binhexa_excep1()); break;
+                        default:  print_r (f_binhexa($converter));
+                    }break;
             }
         }
     }
